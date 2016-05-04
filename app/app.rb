cebbff482040f0267cbe16961c0b6a3bf5caf758
Matthew Ward <iammatthewward@gmail.com>
2016-05-04 15:54:27 +0100

    Database URL removed

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,4 @@
 ENV["RACK_ENV"] ||= "development"
-ENV["DATABASE_URL"] ||= "postgres://ushgavrfmajwyg:7op03RrRTX1_Z4O03YH2ddWqwc@ec2-107-20-174-127.compute-1.amazonaws.com:5432/dljb8mipjksfo"
 
 require 'sinatra/base'
 require './app/models/link'

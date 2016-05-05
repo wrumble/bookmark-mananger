fe52c6ecf6e587dfd512b286b69d16a21f53fe39
Wayne Rumble <rumblewayne@gmail.com>
2016-05-05 11:19:05 +0100

    filter links by tag test passed

--- app/app.rb
+++ app/app.rb
@@ -29,5 +29,11 @@ class BookmarkManager < Sinatra::Base
     redirect :'links'
   end
 
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
   run! if app_file == $0
 end

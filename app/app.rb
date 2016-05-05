401de10617759b6445bfa8201b480d9d3278c401
Hanna <hannaeberhoefer@yahoo.com>
2016-05-05 11:40:22 +0100

    Added rake

--- app/app.rb
+++ app/app.rb
@@ -21,10 +21,11 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(url: params[:url],
+    link = Link.create(url: params[:url],
       title: params[:title])
-    tag = Tag.create(name: params[:tags])
-    link.tags << tag
+    params[:tags].split.each do |tag|
+      link.tags << Tag.create(name: tag)
+    end
     link.save
     redirect :'links'
   end
--- spec/features/adding_tag_spec.rb
+++ spec/features/adding_tag_spec.rb
@@ -9,4 +9,15 @@ feature 'Adding tags' do
     link = Link.first
     expect(link.tags.map(&:name)).to include('Animals')
   end
+
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags', with: 'education ruby'
+    click_button 'Add link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
+  end
 end
+

0a021393ef5d200334ade0f56b9689c7d3962464
Hanna <hannaeberhoefer@yahoo.com>
2016-05-05 15:13:46 +0100

    Added password confirmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,4 +6,17 @@ feature 'User signs up' do
     expect(User.first.email).to eq('stuff@what.com')
   end
 
+  scenario 'requires a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    end
+    def sign_up(email: 'stuff@what.com',
+             password: 'bananas',
+             password_confirmation: 'bananas')
+
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
 end
